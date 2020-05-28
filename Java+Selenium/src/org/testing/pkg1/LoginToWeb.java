7cd3803604ef2290aaab00b6638ee685197e98e7
Admin <Admin@10.28.8.38>
2020-05-28 10:34:12 +0530

    Task1 completed

--- Java+Selenium/src/org/testing/pkg1/LoginToWeb.java
+++ Java+Selenium/src/org/testing/pkg1/LoginToWeb.java
@@ -7,22 +7,12 @@ public class LoginToWeb
 	public static void main(String[] args) throws InterruptedException
 	{
 	WebDriver web =new FirefoxDriver();
-	web.get("https://www.facebook.com");
+	web.get("http://www.demo.guru99.com/V4/");
 	web.manage().window().maximize();
-	
-	//Create Account on Facebook
-	web.findElement(By.name("firstname")).sendKeys("Namita");
-	web.findElement(By.name("lastname")).sendKeys("Mishra");
-	web.findElement(By.name("reg_email__")).sendKeys("amitnamita199512@gmail.com");
-	web.findElement(By.name("reg_passwd__")).sendKeys("amit@1234");
-	web.findElement(By.xpath("//select[@id='day']/option[@value='10']")).click();
-	web.findElement(By.xpath("//select[@id='month']/option[@value='4']")).click();
-	web.findElement(By.xpath("//select[@id='year']/option[@value='1996']")).click();
-	web.findElement(By.name("reg_email_confirmation__")).sendKeys("amitnamita199512@gmail.com");
-	web.findElement(By.xpath("//span[@id='u_0_z']/span[1]/input[1]")).click();
-	web.findElement(By.id("u_0_13")).click();
-
-
+    web.findElement(By.name("uid")).sendKeys("Userld");
+    web.findElement(By.name("password")).sendKeys("Password");
+    web.findElement(By.name("btnLogin")).click();
+    System.out.println("Login Successful");
 	}
 
 	
